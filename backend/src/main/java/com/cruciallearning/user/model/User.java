56880c1a7e4a2b35db34c0945301272b33609c45
Sam Berg <stberg3@gmail.com>
2023-02-03 18:04:00 -0500

    Changed ID to long for typing convenience

--- backend/src/main/java/com/cruciallearning/user/model/User.java
+++ backend/src/main/java/com/cruciallearning/user/model/User.java
@@ -3,16 +3,14 @@ package com.cruciallearning.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -21,19 +19,9 @@ import java.util.UUID;
 public class User {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator",
-            parameters = {
-                    @Parameter(
-                            name = "uuid_gen_strategy_class",
-                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
-                    )
-            }
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", updatable = false, nullable = false)
-    private UUID userId;
+    private long userId;
 
     @NonNull
     @Column(name = "FIRST_NAME")
--- backend/src/main/java/com/cruciallearning/user/repository/UserRepository.java
+++ backend/src/main/java/com/cruciallearning/user/repository/UserRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @RestResource
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
