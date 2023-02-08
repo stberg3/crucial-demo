767deed0c57d78561bb22eb401dcc179589d2791
Berg <sam.berg@bestbuy.com>
2023-02-08 09:29:18 -0600

    Fixing persistent issues (ID wtf)

--- backend/src/main/java/com/cruciallearning/user/model/User.java
+++ backend/src/main/java/com/cruciallearning/user/model/User.java
@@ -10,18 +10,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "APP_USER")
+@Entity
+@Table(name = "APP_USER")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", updatable = false, nullable = false)
-    private long userId;
+    @Column(name = "ID", updatable = false, nullable = false, unique = true)
+    private Long userId;
 
     @NonNull
     @Column(name = "FIRST_NAME")
--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -9,10 +9,10 @@ class ShoesizeForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      birthdate: '',
-      shoeSize: '',
+      firstName: 'Fake',
+      lastName: 'Person',
+      birthdate: '1990-03-13',
+      shoeSize: '12',
       submitEnabled: false
     };
 
@@ -28,37 +28,37 @@ class ShoesizeForm extends React.Component {
     this.setState({ 
       [name]: value
     });
-
-    // this.setState({ 
-    //   submitEnabled: this.state.birthdate != null && 
-    //                   this.state.firstName.trim.length > 0 && 
-    //                   this.state.lastName.trim.length > 0 && 
-    //                   this.state.shoeSize > 0
-    // });
-    // console.log(`${name}:${value}`)
-    // console.log(`birthdate != null: ${this.state.birthdate != null} [${this.state.birthdate}]`)
-    // console.log(`firstName.trim.length > 0: ${this.state.firstName.trim.length > 0} [${this.state.firstName}]`)
-    // console.log(`lastName.trim.length > 0: ${this.state.lastName.trim.length > 0} [${this.state.lastName}]`)
-    // console.log(`shoeSize > 0: ${this.state.shoeSize > 0} [${this.state.shoeSize}]`)
-    // console.log(`submitEnabled > 0: ${this.state.submitEnabled}`)
   }
   
-
   handleSubmit(event) {
-    alert('A form was submitted: ' + this.state.value);
+    const userData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      birthdate: this.state.birthdate,
+      shoeSize: this.state.shoeSize,
+    }
+    
+    console.log(JSON.stringify(userData))
+
+    fetch('/users', {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+    ).then((r) => {
+      console.log(JSON.stringify(r))
+    })
+
     event.preventDefault();
   }
 
   render() {
-    const submitEnabled = this.state.birthdate != null && 
+    const submitEnabled = new Date(this.state.birthdate) < new Date() && 
                           this.state.firstName.trim().length > 0 && 
                           this.state.lastName.trim().length > 0 && 
-                          this.state.shoeSize > 0
-    console.log(submitEnabled)
-    console.log(`birthdate: [${new Date(this.state.birthdate)}] => ${new Date(this.state.birthdate) < new Date()}`)
-    console.log(`firstName: [${this.state.firstName}] => ${this.state.firstName.trim().length > 0}`)
-    console.log(`lastName: [${this.state.lastName}] => ${this.state.lastName.trim().length > 0}`)
-    console.log(`shoeSize: [${this.state.shoeSize}] => ${this.state.shoeSize > 0}`)
+                          this.state.shoeSize > 0;
 
     return (
       <form onSubmit={this.handleSubmit} style={{
@@ -92,39 +92,39 @@ class ShoesizeForm extends React.Component {
 
 const App = () => {
 
- const gridRef = useRef(); // Optional - for accessing Grid's API
- const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
 
- // Each Column Definition results in one Column.
- const [columnDefs, setColumnDefs] = useState([
-   {field: 'birthdate', filter: true},
-   {field: 'shoeSize', filter: true},
-   {field: 'name'}
- ]);
+  // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'birthdate', filter: true},
+    {field: 'shoeSize', filter: true},
+    {field: 'name'}
+  ]);
 
 
- // DefaultColDef sets props common to all Columns
- const defaultColDef = useMemo( ()=> ({ sortable: true }));
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo( ()=> ({ sortable: true }));
 
- // Example of consuming Grid Event
- const cellClickedListener = useCallback( event => {
-   console.log('cellClicked', event);
- }, []);
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback( event => {
+    console.log('cellClicked', event);
+  }, []);
 
-// Example load data from sever
- useEffect(() => {
-   fetch('/users')
-   .then(result => result.json())
-   .then(rowData => setRowData(rowData._embedded.users))
- }, []);
+  // Example load data from sever
+  useEffect(() => {
+    fetch('/users')
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData._embedded.users))
+  }, []);
 
- // Example using Grid's API
- const buttonListener = useCallback( e => {
-   gridRef.current.api.deselectAll();
- }, []);
+  // Example using Grid's API
+  const buttonListener = useCallback( e => {
+    gridRef.current.api.deselectAll();
+  }, []);
 
- return (
-   <div style={{
+  return (
+    <div style={{
         display: 'grid',
         gridTemplateColumns: "repeat(3, 1fr)",
         gap: '10px',
@@ -133,29 +133,29 @@ const App = () => {
 
     <ShoesizeForm style={{gridColumn: 1}}/>
 
-     {
-     /* 
-     On div wrapping Grid 
+      {
+      /* 
+      On div wrapping Grid 
         a) specify theme CSS Class Class and 
         b) sets Grid size
       */
-     }
-     <div className="ag-theme-alpine" style={{height: 500, gridColumn: '2 / 4'}}>
-       <AgGridReact
-           ref={gridRef} // Ref for accessing Grid's API
-           rowData={rowData} // Row Data for Rows
+      }
+      <div className="ag-theme-alpine" style={{height: 500, gridColumn: '2 / 4'}}>
+        <AgGridReact
+            ref={gridRef} // Ref for accessing Grid's API
+            rowData={rowData} // Row Data for Rows
 
-           columnDefs={columnDefs} // Column Defs for Columns
-           defaultColDef={defaultColDef} // Default Column Properties
+            columnDefs={columnDefs} // Column Defs for Columns
+            defaultColDef={defaultColDef} // Default Column Properties
 
-           animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-           rowSelection='multiple' // Options - allows click selection of rows
+            animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+            rowSelection='multiple' // Options - allows click selection of rows
 
-           onCellClicked={cellClickedListener} // Optional - registering for Grid Event
-           />
-     </div>
-   </div>
- );
+            onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+            />
+      </div>
+    </div>
+  );
 };
 
 export default App;
